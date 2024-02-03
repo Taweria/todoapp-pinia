bc650c0370038bd3a57e0ade26f23fa30cbde4a1
Taweria <elodieali.pro@gmail.com>
2024-02-03 17:40:13 +0100

    Quick Fix

--- todo-app/src/components/TaskForm.vue
+++ todo-app/src/components/TaskForm.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { v4 as uuid } from 'uuid'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { useTasksStore } from '../store/tasks';
 
-const name = ref('')
-const description = ref('');
+const name = computed(() => taskStore.selectedTask ? taskStore.selectedTask.name : '');
+const description = computed(() => taskStore.selectedTask ? taskStore.selectedTask.description : '');
 const taskStore = useTasksStore();
 
 function handleFormSubmit() {
